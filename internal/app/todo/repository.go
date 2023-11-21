f4f8cdd800cae59d63095f1f753df920e326966f
liviudnicoara <119079662+liviudnicoara@users.noreply.github.com>
2023-11-21 10:43:08 +0200

    Update repository.go

--- internal/app/todo/repository.go
+++ internal/app/todo/repository.go
@@ -1,3 +1,5 @@
+// Package todo provides functionality related to todo management.
+
 package todo
 
 import (
@@ -5,10 +7,12 @@ import (
 	"github.com/liviudnicoara/go-graphql-poc/shared"
 )
 
+// TodoRepository defines the interface for todo-related database operations.
 type TodoRepository interface {
 	storage.GenericRepository[shared.Todo]
 }
 
-func NewToDoRepository() TodoRepository {
+// NewTodoRepository creates a new TodoRepository instance using the shared.Todo type.
+func NewTodoRepository() TodoRepository {
 	return storage.NewGenericRepository[shared.Todo]()
 }
